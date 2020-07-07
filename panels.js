703cff2ffd365b1b123fa97b365f8a402938bd92
ThomasDenoncin <thomas.denoncin@gmail.com>
2020-07-07 22:10:50 +0200

    feature(loader): add loader

--- panels.js
+++ panels.js
@@ -2,47 +2,53 @@
     const Panels = document.querySelector('.panels');
     const Timer = document.querySelector('.timer');
     const Score = document.querySelector('.score');
+    const GamesList = document.querySelector('.game-selector > select');
+    const Games = {
+        footBallGame : function() {
+            updateScore(0);
+            generateGrid(true);
+            
+            launchTimer(15, () => {
+                document.querySelectorAll('.panel').forEach((panel) => {
+                    panel.onclick = null;
+                });
+                Score.classList.add('blink');
+            })
+            
+            document.querySelectorAll('.panel').forEach((panel) => {
+                panel.classList.add('red');
+                
+                panel.onclick = (e) => {
+                    const target = e.target;
+                    if (target.classList.contains('red')) {
+                        updateScore(1);
+                        target.classList.remove('red');
+                        
+                        if (document.querySelectorAll('.panel.red').length == 0) {
+                            resetPanels(() => {
+                                document.querySelectorAll('.panel').forEach((panel) => {
+                                    panel.classList.add('red');
+                                });
+                            });
+                        }
+                    }
+                }
+            })
+        }
+    }
+    
     let score = 0;
 
+    function launchGame(game) {
+        Games[game]();
+    }
+    
     for (let i = 0; i < 16; i++) {
         const panel = document.createElement('div');
         panel.classList.add('panel');
         Panels.appendChild(panel);
     }
-
-    launchFootBallGame();
-
-    function launchFootBallGame() {
-        updateScore(0);
-        
-        launchTimer(15, () => {
-            document.querySelectorAll('.panel').forEach((panel) => {
-                panel.onclick = null;
-            });
-            Score.classList.add('blink');
-        })
-
-        document.querySelectorAll('.panel').forEach((panel) => {
-            panel.classList.add('red');
-
-            panel.onclick = (e) => {
-                const target = e.target;
-                if (target.classList.contains('red')) {
-                    updateScore(1);
-                    target.classList.remove('red');
-
-                    if (document.querySelectorAll('.panel.red').length == 0) {
-                        resetPanels(() => {
-                            document.querySelectorAll('.panel').forEach((panel) => {
-                                panel.classList.add('red');
-                            });
-                        });
-                    }
-                }
-            }
-        })
-    }
-
+    
     function updateScore(value) {
         score += value;
         Score.innerHTML = score;
@@ -62,6 +68,25 @@
 
     function resetPanels(reset) {
         reset();
+    }
+
+    function generateGrid(reset = false) {
+        if (reset) {
+            let panelList = Panels.querySelectorAll('.panel');
+            panelList.forEach(function(panel){
+                panel.remove();
+            });
+        }
+
+        for (let i = 0; i < 16; i++) {
+            const panel = document.createElement('div');
+            panel.classList.add('panel');
+            Panels.appendChild(panel);
+        }        
+    }
+
+    document.querySelector('.loader').onclick = function() {
+        launchGame(GamesList.options[GamesList.selectedIndex].value);
     }
  
  })();
